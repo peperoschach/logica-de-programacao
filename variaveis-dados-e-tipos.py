ef32704a009623aaeac1ba8fe2993b4af8d1b216
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-05 21:33:55 -0300

    variáveis, dados e seus tipos

---
+++ variaveis-dados-e-tipos.py
@@ -0,0 +1,22 @@
+nota = 8.5
+disciplina = 'Lógica de Programação e Algoritmos'
+
+print('Disciplina: ', disciplina, '. Nota: ', nota) # Lógica de Programaçãoo e Algoritmos . Nota:  8.5
+
+""" Variável Lógica """
+
+a = 1 #a recebe 1
+b = 5 #b recebe 5
+
+resposta = a == b
+print(resposta) #FALSE
+
+resposta = a != b
+print(resposta) #TRUE
+
+""" Variável String """
+
+frase = 'Olá Mundo!'
+print(frase) #Olá Mundo!
+print(frase[0]) #O
+print(frase[2]) #á
