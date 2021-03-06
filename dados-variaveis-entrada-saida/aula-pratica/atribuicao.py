5c787a8dca7a4fef82682d442b2338aeec29ef8c
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-05 23:57:44 -0300

    Aula prática: Expressões algébricas

---
+++ dados-variaveis-entrada-saida/aula-pratica/atribuicao.py
@@ -0,0 +1,7 @@
+""" Atribuição """
+
+'''
+Escreva as expressões em Python para:
+
+a) Atribuir um valor inteiro a variáv
+'''
---
+++ dados-variaveis-entrada-saida/aula-pratica/expressoes-algebricas.py
@@ -0,0 +1,41 @@
+""" Expressões algébricas """
+
+'''
+Escreva as seguintes expressões algébricas em liguagem Python:
+
+a) O somatório dos 5 primeiros núemros inteiros e positivos
+b) A média entre 23, 19 e 31
+c) O número de vezes que 73 cabe em 403
+d) A sobra quando 403 é dividido por 73
+e) 2 eleveado a 10ª potência
+f) O valor absoluto da diferença entre 54 e 57
+g) O menor valor entre 34, 29 e 31
+'''
+# A
+a_soma = 1 + 2 + 3 + 4 + 5
+print(a_soma)
+
+#B
+b_media = (23 + 19 + 31) / 3
+print(b_media)
+
+#C
+c_numero = 403 // 73
+print(c_numero)
+
+#D
+d_sobra = 403 % 73
+print(d_sobra)
+
+#E
+e_potencia = 2**10
+print(e_potencia)
+
+#F
+f_valor_absoluto = abs(54 - 57)
+print(f_valor_absoluto)
+
+#G
+g_menor_valor = min(34, 29, 31)
+print(g_menor_valor)
+
