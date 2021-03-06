4c4f1c3183764f893028431c247ef183a42f369c
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-06 00:09:30 -0300

    Aula prática: Atribuição

--- dados-variaveis-entrada-saida/aula-pratica/atribuicao.py
+++ dados-variaveis-entrada-saida/aula-pratica/atribuicao.py
@@ -3,5 +3,16 @@
 '''
 Escreva as expressões em Python para:
 
-a) Atribuir um valor inteiro a variáv
+a) Atribuir um valor inteiro 3 à variável a
+b)Atribuir o valor 4 à variável b
+c) Atribuir à variável c o valor da expressão a*a + b*b
 '''
+
+#A
+a = 3
+
+#B
+b = 4
+
+#C
+c = a*a + b*b
