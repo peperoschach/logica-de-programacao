8f5191dcd80444632a989aee1078eacae6802e4c
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-12 18:17:20 -0300

    Condicionais Simples

---
+++ estruturas-condicionais/condicional-simples.py
@@ -0,0 +1,7 @@
+# Lê dois valores inteiros e compara ambos
+
+x = int(input('Digite um valor inteiro: '))
+y = int(input('Digite um segundo valor inteiro: '))
+
+if (x > y):
+    print('O primeiro valor é maior que o segundo!')
