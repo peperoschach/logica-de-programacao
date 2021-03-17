23e5ecda8dbf260dc96473e65efc7705a140f504
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-17 20:08:33 -0300

    Valores Trhuthy e Falsey

---
+++ estruturas-de-repeticao/valores-trhuty-falsey.py
@@ -0,0 +1,8 @@
+nome = ''
+while not nome:
+    nome = input('Digite seu nome: ')
+valor = int(input('Digite um número qualquer: '))
+if (valor):
+    print('Você digitou um valor diferente de zero')
+else:
+    print('Você digitou ZERO.')
