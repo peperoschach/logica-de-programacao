d2dcd0ffe222962292eb2b35b87f4df7ad502b54
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-18 21:38:16 -0300

    retorno de variáveis em funções

---
+++ funcoes/retorno-de-variaveis.py
@@ -0,0 +1,17 @@
+def soma3(x= 0, y = 0, z = 0):
+    res = x + y + z
+    return res
+
+#Programa Principal
+retornado =  soma3(1,2,3)
+print(retornado)
+
+#forma alternativa simplificada
+print(soma3(2,2))
+
+#programa principal II
+retornado1 = soma3(1,2,3)
+retornado2 = soma3(1,2)
+retornado3 = soma3()
+
+print('Somatórios: {}, {} e {}'.format(retornado1, retornado2, retornado3))
