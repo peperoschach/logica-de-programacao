e679c255a11f92fe1020af5d3a464dd4fe1bf3bb
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-20 14:36:47 -0300

    Exercicio - Fatorial

---
+++ funcoes/exercicios/fatorial.py
@@ -0,0 +1,14 @@
+def fatorial(num):
+    fat = 1
+    if (num == 0):
+        return fat
+
+    for i in range(1, num+1, 1):
+        fat *= i # fat = fat * i
+    
+    return fat
+
+
+#Programa principal
+x = int(input('Digite um valor para calcular a fatorial: -- '))
+print('{}! = {}'.format(x, fatorial(x)))
