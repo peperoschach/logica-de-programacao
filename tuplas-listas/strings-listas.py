83691fc11c089e69a705f5b0de1f79740e6160bf
peperoschach <luiz.felipe_23@yahoo.com.br>
2021-03-24 21:23:29 -0300

    Acessando strings dentro de listas

--- tuplas-listas/strings-listas.py
+++ tuplas-listas/strings-listas.py
@@ -10,3 +10,10 @@ for item in mochila:
     for letra in item:
         print(letra, end='')
     print()
+
+
+for i in range(0, len(mochila),1):
+    for j in range(0,len(mochila[i]),1):
+        print(mochila[i][j], end='')
+    print()
+
